fbacdf02f38ae6c0ac3d704f579ac79df4dd2ec0
Armandase <adamiens@student.42angouleme.fr>
2025-03-14 17:48:45 +0100

    fix implementation, but B 1 and B2 are not the same idk why but ok. Still need to verify the implementation

--- total-perspective-vortex/CustomCSP.py
+++ total-perspective-vortex/CustomCSP.py
@@ -6,27 +6,11 @@ from sklearn.base import TransformerMixin
 #   using a spatial filter. It applies a spatial filter to the multi-channel EEG data
 #   to enhance the signal variance of one class while reducing it for the other within the same domain.
 class CustomCSP(TransformerMixin):
-    def __init__(self, n_components=4, reg=None, log=True, norm_trace=False, cov_est="concat", cov_method_params=None, transform_into="average_power", rank=None):
+    def __init__(self, n_components=4):
         self.n_components = n_components
-        self.reg = reg
-        self.log = log
-        self.norm_trace = norm_trace
-        self.cov_est = cov_est
-        self.cov_method_params = cov_method_params
-        self.transform_into = transform_into
-        self.rank = rank
+        self.filters = None
         self.name = 'CustomCSP'
-    
-    def time_avg_covariance(self, X):
-        # n_trials, n_channels, n_samples = X.shape
-        # cov = np.zeros((n_channels, n_channels))
-        # for i in range(n_trials):
-        #     cov += np.dot(X[i], X[i].T)
-        # return cov / n_trials
-        
-        cov = X @ X.T
-        
-        return cov / X.shape[1]
+        self.is_fit_ = False
         
     def normalized_spatial_covariance(self, X):
         """
@@ -37,7 +21,6 @@ class CustomCSP(TransformerMixin):
          
          return: cov (n_channels, n_channels)
         """
-        # cov = np.dot(X.T, X)
         cov = np.dot(X, X.T)
         return cov / np.trace(cov)
     
@@ -74,85 +57,60 @@ class CustomCSP(TransformerMixin):
         for x_class in list_class:
             avg_class.append(np.mean(x_class, axis=0))
         for i in range(X.shape[0]):
-            X[i] = self.decentre_data(X[i], avg_class[y[i]])
+            X[i] = self.decentre_data(X[i], avg_class[y[i]]) # En
         
         decentred_list_class = self.extract_class(X, y)
         norm_spatial_cov = []
         for decentred_class in decentred_list_class:
-            norm_spatial_cov.append(self.population_covariance(decentred_class))
+            norm_spatial_cov.append(self.population_covariance(decentred_class)) # (Cn)
         
         #compostite qui signifie que c'est la combinaison de plusieurs elements
-        composite_spatial_cov = np.sum(norm_spatial_cov, axis=0)
+        composite_spatial_cov = np.sum(norm_spatial_cov, axis=0) # (Cc)
         # decomposition en valeurs propres
         # les valeurs propres sont egales aux racines du polynome caracteristique de la matrice
-        eigvals, eigvecs = np.linalg.eig(composite_spatial_cov)
+        eigvals, eigvecs = np.linalg.eig(composite_spatial_cov) # (V)
         # diagonal matrix of eigenvalues sorted in descending order
         sorted_eigvals = np.sort(eigvals)[::-1]
-        diag = np.diag(sorted_eigvals)
+        diag = np.diag(sorted_eigvals) # (D)
         
-        print('Diagonal:', diag.shape)
-        print('Eigvals:', eigvals.shape)
-        print('eigvecs:', eigvecs.shape)
         
-        # whitening matrix
+        # whitening matrix (P)
         whitening = np.dot(np.sqrt(np.linalg.inv(diag)), eigvecs.T)
-        print('Whitening:', whitening.shape)
         
         # whitening performed on the spatial covariance matrices
-        spatial_cov_whitened = []
+        spatial_cov_whitened = [] # PCnP′
         for cov in norm_spatial_cov:
             spatial_cov_whitened.append(np.dot(np.dot(whitening, cov), whitening.T))
 
-        whitening_eigvecs = []
-        # whitening_diags = []
+        whitening_eigvecs = [] # BΛnB′
         for cov in spatial_cov_whitened:
             eigvals, eigvecs = np.linalg.eig(cov)
             sorted_eigvals = np.sort(eigvals)[::-1]
             whitening_eigvecs.append(eigvecs)
-            # whitening_diags.append(np.diag(sorted_eigvals))
         
-        # spatial filter
+        # spatial filter (W)
         spatial_filter = []
         for eigvecs in whitening_eigvecs:
             spatial_filter.append(np.dot(eigvecs.T, whitening))
 
-        # apply spatial filter to the data
-        # X_filtered = []
-        # for i in range(X.shape[0]):
-        #     X_filtered.append(np.dot(spatial_filter[y[i]], X[i]))
-        #     print('X_filtered:', X_filtered[-1].shape)
-        # exit(1)
+        # self.filters = np.array(spatial_filter)
+        self.filters = np.array(spatial_filter[:self.n_components])
+        
+        self.is_fit_ = True
         return self
 
     def transform(self, X):
-        # Xcsp(t) = Wtranspos * x(t)
-        
-        # Xcsp = AS 
-        # ou    A est une matrice de mélange qui contient
-        #           les coefficients indiquant comment chaque source
-        #           contribue aux des dimensions de l'observation
-        #       S correspond aux signaux sources
-        
-        # On condisere que chaque colonne de X et S sont des samples temporelles:
-        # x(t) = As(t)
-        
-        # La matrice A joue un role essentiel dans la maniere
-        #   dont les statitques des signaux obesrvés x(t) se retrouvent
-        #   liées a celle des sources originales s(t)
-        # L’effet de la matrice de mélange A peut être vu comme  transformation
-        #   qui prend les statistiques simples (diagonales) des sources et 
-        #   les « étend » pour expliquer les statistiques croisées plus riches observées dans x(t).
-        # On trouve donc l'equation suivante:
-        # x(t)x(t)transpos = As(t) * (As(t))transpos
-        #                  = As(t) * s(t)transpos * Atranspos
-        #                  = ARsAtranspos
+        X_transform = X.copy()
+        # apply spatial filter to the data
 
-        if self.is_fitted_ is False:
-            raise Exception("Model not fitted")
-        X = X.copy()
-        
-        
-        return X
+        if self.is_fit_ is False:
+            # raise ValueError("The model is not fitted yet")
+            print("The model is not fitted yet")
+            return X_transform
+
+        print('Apply CSP')        
+        X_transform = np.asarray([np.dot(self.filters, x) for x in X_transform])
+        return X_transform
     
     def fit_transform(self, X, y):
         X = self.transform(X.copy())
